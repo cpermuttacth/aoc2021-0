b95e7facecfcc18d4cc4d262a589d0c32bea3597
mats cronqvist <masse@cronqvi.st>
2020-12-01 12:27:43 +0100

    day 1

---
+++ 01/t1.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+double_ptr() {
+    local a=$1 b=$2 sum=$3
+    while ((a < b))
+    do ((A[a] + A[b] == sum)) && echo $((A[a] * A[b]))
+       if ((A[a] + A[b] < sum))
+       then ((a++))
+       else ((b--))
+       fi
+    done
+}
+
+# get data
+mapfile -t A < <(sort -n in.txt)
+d=$((${#A[@]} - 1))
+
+# task #1
+double_ptr 0 "$d" 2020
+
+# task #2
+while ((0 < d--))
+do s="$(double_ptr 0 "$d" $((2020 - A[d])))"
+   [ -n "$s" ] && echo $((A[d] * s))
+done
